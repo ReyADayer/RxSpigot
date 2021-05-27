47172d567036aebbe19d995fd46c3ba7ed80ae77
ReyADayer <reyaddayer@gmail.com>
2021-05-28 04:04:31 +0900

    :art: update workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,15 +29,59 @@ buildscript {
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "net.atlantis"
-                artifactId = "rxspigot"
-                version = project.version.toString()
-                from(components["kotlin"])
+publishing {
+    publications {
+        create<MavenPublication>("root") {
+            groupId = "net.atlantis"
+            artifactId = "rxspigot"
+            version = project.version.toString()
+            from(components["kotlin"])
+        }
+    }
+    repositories {
+        githubPackages("ReyADayer/RxSpigot")
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
             }
         }
+    }
+}
+
+tasks {
+    compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+        options.encoding = "UTF-8"
+    }
+
+    compileTestJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+}
+
+fun RepositoryHandler.githubPackages(path: String) = maven {
+    url = uri("https://maven.pkg.github.com/$path")
+    name = "GithubPackages"
+    credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
     }
 }
